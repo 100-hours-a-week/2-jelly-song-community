5a9b12a54d3ad67ccf9b347f5b7d20f9cef0a0b5
jeli01 <jeli01@naver.com>
2025-02-13 15:39:21 +0900

    initial commit

---
+++ src/main/java/io/github/jeli01/kakao_bootcamp_community/KakaoBootcampCommunityApplication.java
@@ -0,0 +1,13 @@
+package io.github.jeli01.kakao_bootcamp_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoBootcampCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoBootcampCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jeli01/kakao_bootcamp_community/KakaoBootcampCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jeli01.kakao_bootcamp_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoBootcampCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
