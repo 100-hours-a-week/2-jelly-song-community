fdb0c79a5c13f796066806fc7016978d1b07e0b6
jeli01 <jeli01@naver.com>
2025-02-17 11:39:59 +0900

    refactor(login-script.js): 추상성 내려가게 리팩토링

--- src/main/resources/static/js/login-script.js
+++ src/main/resources/static/js/login-script.js
@@ -11,44 +11,59 @@ let $login_button = document.querySelector(".button-disable");
 let email_regex = /^[a-zA-Z0-9.]+@[a-zA-Z0-9.]+\.[a-zA-Z]{2,4}$/i;
 let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
 
-$layout_form.addEventListener("submit", (event) => {
-    let email_address = $email_form.value;
-    let password = $password_form.value;
+preventIfNotValidated();
+validateWheneverTyped();
 
-    if (email_regex.test(email_address) && passwordRegex.test(password)) {
-        return;
-    }
-    event.preventDefault();
-})
+function preventIfNotValidated() {
+    $layout_form.addEventListener("submit", (event) => {
+        let email_address = $email_form.value;
+        let password = $password_form.value;
 
-$layout_form.addEventListener("input", (event) => {
-    let email_address = $email_form.value;
-    let password = $password_form.value;
+        if (email_regex.test(email_address) && passwordRegex.test(password)) {
+            return;
+        }
+        event.preventDefault();
+    })
+}
+function validateWheneverTyped() {
+    $layout_form.addEventListener("input", (event) => {
+        let email_address = $email_form.value;
+        let password = $password_form.value;
+        let validation = true;
 
-    let validation = true;
+        validateEmail();
+        validatePassword();
+        validateButton();
 
-    if (!email_regex.test(email_address)) {
-        email_validation_container.innerText = "*올바른 이메일 주소 형식을 입력해주세요";
-        validation = false;
-    } else {
-        email_validation_container.innerText = "";
-    }
+        function validateEmail() {
+            if (!email_regex.test(email_address)) {
+                email_validation_container.innerText = "*올바른 이메일 주소 형식을 입력해주세요";
+                validation = false;
+            } else {
+                email_validation_container.innerText = "";
+            }
+        }
 
-    if ($password_form.value == "") {
-        $password_validation_container.innerText = "*비밀번호를 입력해주세요"
-        validation = false;
-    } else if (!passwordRegex.test(password)) {
-        $password_validation_container.innerText = "*비밀번호는 8자 이상, 20자이하이며, 대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다.";
-        validation = false;
-    } else {
-        $password_validation_container.innerText = "";
-    }
+        function validatePassword() {
+            if ($password_form.value == "") {
+                $password_validation_container.innerText = "*비밀번호를 입력해주세요"
+                validation = false;
+            } else if (!passwordRegex.test(password)) {
+                $password_validation_container.innerText = "*비밀번호는 8자 이상, 20자이하이며, 대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다.";
+                validation = false;
+            } else {
+                $password_validation_container.innerText = "";
+            }
+        }
 
-    if (validation == true) {
-        $login_button.classList.remove("button-disable")
-        $login_button.classList.add("button-enable")
-    } else {
-        $login_button.classList.remove("button-enable")
-        $login_button.classList.add("button-disable")
-    }
-})
+        function validateButton() {
+            if (validation == true) {
+                $login_button.classList.remove("button-disable")
+                $login_button.classList.add("button-enable")
+            } else {
+                $login_button.classList.remove("button-enable")
+                $login_button.classList.add("button-disable")
+            }
+        }
+    })
+}
