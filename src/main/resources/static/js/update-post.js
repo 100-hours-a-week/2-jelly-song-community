bf59d6b657240fb04e6a00ced2cffd23471ea3c7
jeli01 <jeli01@naver.com>
2025-02-18 22:45:37 +0900

    refactor(update-post.js) 추상성 내려가도록 리팩토링

--- src/main/resources/static/js/update-post.js
+++ src/main/resources/static/js/update-post.js
@@ -1,13 +1,22 @@
 let $header_back = document.querySelector(".header-back");
-
-$header_back.addEventListener("click", (event) => {
-    window.location.href = "./post.html";
-})
-
 let update_post_container_form = document.querySelector(".update-post-container-form");
 let $button = document.querySelector(".button-disable");
+$email_form = document.querySelector(".email-form");
+$post_comment_form_textarea = document.querySelector(".text-area-form");
+$post_comment_form_button = document.querySelector(".button-disable");
+$password_validation_container = document.querySelector(".password-validation-container");
 
+activateHeaderBack();
 preventSubmitIfNotValidate();
+validateEmail();
+validateTextArea();
+initializeButtonAttribute();
+function activateHeaderBack() {
+    $header_back.addEventListener("click", (event) => {
+        window.location.href = "./post.html";
+    })
+}
+
 function preventSubmitIfNotValidate() {
     update_post_container_form.addEventListener("submit", (event) => {
         if ($button.classList.contains("button-disable")) {
@@ -16,37 +25,35 @@ function preventSubmitIfNotValidate() {
     })
 }
 
-$email_form = document.querySelector(".email-form");
-$post_comment_form_textarea = document.querySelector(".text-area-form");
-$post_comment_form_button = document.querySelector(".button-disable");
-$password_validation_container = document.querySelector(".password-validation-container");
+function validateEmail() {
+    $email_form.addEventListener("input", () => {
+        if ($email_form.value != "" && $post_comment_form_textarea.value != "" && $post_comment_form_button.classList.contains("button-disable")) {
+            $post_comment_form_button.classList.remove("button-disable")
+            $post_comment_form_button.classList.add("button-enable")
+            $password_validation_container.innerHTML = ""
+        } else if (($email_form.value == "" || $post_comment_form_textarea.value == "") && $post_comment_form_button.classList.contains("button-enable")) {
+            $post_comment_form_button.classList.remove("button-enable")
+            $post_comment_form_button.classList.add("button-disable")
+            $password_validation_container.innerHTML = "제목, 내용을 모두 작성해주세요"
+        }
+    })
+}
 
-$email_form.addEventListener("input", () => {
-    if ($email_form.value != "" && $post_comment_form_textarea.value != "" && $post_comment_form_button.classList.contains("button-disable")) {
-        $post_comment_form_button.classList.remove("button-disable")
-        $post_comment_form_button.classList.add("button-enable")
-        $password_validation_container.innerHTML = ""
-    } else if (($email_form.value == "" || $post_comment_form_textarea.value == "") && $post_comment_form_button.classList.contains("button-enable")) {
-        $post_comment_form_button.classList.remove("button-enable")
-        $post_comment_form_button.classList.add("button-disable")
-        $password_validation_container.innerHTML = "제목, 내용을 모두 작성해주세요"
-    }
-})
+function validateTextArea() {
+    $post_comment_form_textarea.addEventListener("input", () => {
+        if ($email_form.value != "" && $post_comment_form_textarea.value != "" && $post_comment_form_button.classList.contains("button-disable")) {
+            $post_comment_form_button.classList.remove("button-disable")
+            $post_comment_form_button.classList.add("button-enable")
+            $password_validation_container.innerHTML = ""
+        } else if (($email_form.value == "" || $post_comment_form_textarea.value == "") && $post_comment_form_button.classList.contains("button-enable")) {
+            $post_comment_form_button.classList.remove("button-enable")
+            $post_comment_form_button.classList.add("button-disable")
+            $password_validation_container.innerHTML = "제목, 내용을 모두 작성해주세요"
+        }
+    })
+}
 
-$post_comment_form_textarea.addEventListener("input", () => {
-    if ($email_form.value != "" && $post_comment_form_textarea.value != "" && $post_comment_form_button.classList.contains("button-disable")) {
-        $post_comment_form_button.classList.remove("button-disable")
-        $post_comment_form_button.classList.add("button-enable")
-        $password_validation_container.innerHTML = ""
-    } else if (($email_form.value == "" || $post_comment_form_textarea.value == "") && $post_comment_form_button.classList.contains("button-enable")) {
-        $post_comment_form_button.classList.remove("button-enable")
-        $post_comment_form_button.classList.add("button-disable")
-        $password_validation_container.innerHTML = "제목, 내용을 모두 작성해주세요"
-    }
-})
-
-haha();
-function haha() {
+function initializeButtonAttribute() {
     if ($email_form.value != "" && $post_comment_form_textarea.value != "" && $post_comment_form_button.classList.contains("button-disable")) {
         $post_comment_form_button.classList.remove("button-disable")
         $post_comment_form_button.classList.remove("button-enable")
