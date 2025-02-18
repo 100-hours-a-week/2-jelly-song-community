940ed9bb6cfb6aad6621df32c64f2be6373ea37c
jeli01 <jeli01@naver.com>
2025-02-19 00:46:17 +0900

    refactor: 게시물 수정 페이지 중복 제거 및 목적에 맞는 이름 부여

--- src/main/resources/static/js/update-post.js
+++ src/main/resources/static/js/update-post.js
@@ -1,10 +1,10 @@
 let $header_back = document.querySelector(".header-back");
 let update_post_container_form = document.querySelector(".update-post-container-form");
 let $button = document.querySelector(".button-disable");
-$email_form = document.querySelector(".email-form");
-$post_comment_form_textarea = document.querySelector(".text-area-form");
+$title_form = document.querySelector(".title-form");
+$post_comment_form_textarea = document.querySelector(".textarea-form");
 $post_comment_form_button = document.querySelector(".button-disable");
-$password_validation_container = document.querySelector(".password-validation-container");
+$content_validation_container = document.querySelector(".content-validation-container");
 
 activateHeaderBack();
 preventSubmitIfNotValidate();
@@ -26,39 +26,39 @@ function preventSubmitIfNotValidate() {
 }
 
 function validateEmail() {
-    $email_form.addEventListener("input", () => {
-        if ($email_form.value != "" && $post_comment_form_textarea.value != "" && $post_comment_form_button.classList.contains("button-disable")) {
+    $title_form.addEventListener("input", () => {
+        if ($title_form.value != "" && $post_comment_form_textarea.value != "" && $post_comment_form_button.classList.contains("button-disable")) {
             $post_comment_form_button.classList.remove("button-disable")
             $post_comment_form_button.classList.add("button-enable")
-            $password_validation_container.innerHTML = ""
-        } else if (($email_form.value == "" || $post_comment_form_textarea.value == "") && $post_comment_form_button.classList.contains("button-enable")) {
+            $content_validation_container.innerHTML = ""
+        } else if (($title_form.value == "" || $post_comment_form_textarea.value == "") && $post_comment_form_button.classList.contains("button-enable")) {
             $post_comment_form_button.classList.remove("button-enable")
             $post_comment_form_button.classList.add("button-disable")
-            $password_validation_container.innerHTML = "제목, 내용을 모두 작성해주세요"
+            $content_validation_container.innerHTML = "제목, 내용을 모두 작성해주세요"
         }
     })
 }
 
 function validateTextArea() {
     $post_comment_form_textarea.addEventListener("input", () => {
-        if ($email_form.value != "" && $post_comment_form_textarea.value != "" && $post_comment_form_button.classList.contains("button-disable")) {
+        if ($title_form.value != "" && $post_comment_form_textarea.value != "" && $post_comment_form_button.classList.contains("button-disable")) {
             $post_comment_form_button.classList.remove("button-disable")
             $post_comment_form_button.classList.add("button-enable")
-            $password_validation_container.innerHTML = ""
-        } else if (($email_form.value == "" || $post_comment_form_textarea.value == "") && $post_comment_form_button.classList.contains("button-enable")) {
+            $content_validation_container.innerHTML = ""
+        } else if (($title_form.value == "" || $post_comment_form_textarea.value == "") && $post_comment_form_button.classList.contains("button-enable")) {
             $post_comment_form_button.classList.remove("button-enable")
             $post_comment_form_button.classList.add("button-disable")
-            $password_validation_container.innerHTML = "제목, 내용을 모두 작성해주세요"
+            $content_validation_container.innerHTML = "제목, 내용을 모두 작성해주세요"
         }
     })
 }
 
 function initializeButtonAttribute() {
-    if ($email_form.value != "" && $post_comment_form_textarea.value != "" && $post_comment_form_button.classList.contains("button-disable")) {
+    if ($title_form.value != "" && $post_comment_form_textarea.value != "" && $post_comment_form_button.classList.contains("button-disable")) {
         $post_comment_form_button.classList.remove("button-disable")
         $post_comment_form_button.classList.remove("button-enable")
         $post_comment_form_button.classList.add("button-enable")
-    } else if (($email_form.value == "" || $post_comment_form_textarea.value == "") && $post_comment_form_button.classList.contains("button-enable")) {
+    } else if (($title_form.value == "" || $post_comment_form_textarea.value == "") && $post_comment_form_button.classList.contains("button-enable")) {
         $post_comment_form_button.classList.remove("button-disable")
         $post_comment_form_button.classList.remove("button-enable")
         $post_comment_form_button.classList.add("button-disable")
