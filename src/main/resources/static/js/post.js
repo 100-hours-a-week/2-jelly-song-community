03782ed8f877201cb9157aa7fc601b0988c1490b
jeli01 <jeli01@naver.com>
2025-02-18 22:37:19 +0900

    refactor: post.js 추상성 내려가도록 리팩토링

--- src/main/resources/static/js/post.js
+++ src/main/resources/static/js/post.js
@@ -1,37 +1,48 @@
 let $header_back = document.querySelector(".header-back");
+const postDeleteModal = document.querySelector('.post-modal');
+const postDeleteModalOpen = document.querySelector('.post-delete-button');
+const postDeleteModalClose = document.querySelector('.post-close_btn');
+const postDeleteConfirmBtn = document.querySelector(".post-confirm_btn");
+let $like_button = document.querySelector(".post-meta-likes-disable");
+let $post_meta_likes_number = document.querySelector(".post-meta-likes-number");
+let $post_comment_form_textarea = document.querySelector(".post-comment-form-textarea");
+let $post_comment_form_button = document.querySelector(".post-comment-form-button");
+let $post_comment_right_update_buttons = document.querySelectorAll(".post-comment-right-update-button")
+const commentModal = document.querySelector('.comment-modal');
+const commentModalOpens = document.querySelectorAll('.post-comment-right-delete-button');
+const commentModalClose = document.querySelector('.comment-close_btn');
+const commentConfirmBtn = document.querySelector(".comment-confirm_btn");
+let post_comment_form = document.querySelector(".post-comment-form");
 
-$header_back.addEventListener("click", (event) => {
-    window.location.href = "./posts.html";
-})
+activateHeaderBack();
+activatePostDeleteModal();
+changeLikeButtonColor();
+activateCommentFormButtonWhenExistsInCommentBox();
+preventSubmitIfNotValidate();
+changeCommentButtonToUpdateButton();
+activateCommentModal();
 
-const modal = document.querySelector('.post-modal');
-const modalOpen = document.querySelector('.post-delete-button');
-const modalClose = document.querySelector('.post-close_btn');
-const confirm_btn = document.querySelector(".post-confirm_btn");
-
-activateModal();
-
-function activateModal() {
-    modalOpen.addEventListener('click', function () {
-        modal.style.display = 'block';
+function activateHeaderBack() {
+    $header_back.addEventListener("click", (event) => {
+        window.location.href = "./posts.html";
+    })
+}
+function activatePostDeleteModal() {
+    postDeleteModalOpen.addEventListener('click', function () {
+        postDeleteModal.style.display = 'block';
         document.body.style.overflow = 'hidden'
     });
-    modalClose.addEventListener('click', function () {
-        modal.style.display = 'none';
+    postDeleteModalClose.addEventListener('click', function () {
+        postDeleteModal.style.display = 'none';
         document.body.style.overflow = ''
     });
 
-    confirm_btn.addEventListener('click', function () {
+    postDeleteConfirmBtn.addEventListener('click', function () {
         location.href = "./posts.html";
         document.body.style.overflow = ''
     })
 }
 
-
-$like_button = document.querySelector(".post-meta-likes-disable");
-$post_meta_likes_number = document.querySelector(".post-meta-likes-number");
-
-changeLikeButtonColor();
 function changeLikeButtonColor() {
     $like_button.addEventListener("click", () => {
         if ($like_button.classList.contains("post-meta-likes-disable")) {
@@ -41,42 +52,23 @@ function changeLikeButtonColor() {
         } else {
             $like_button.classList.remove("post-meta-likes-enable")
             $like_button.classList.add("post-meta-likes-disable")
-            $post_meta_likes_number.innerHTML = Number($post_meta_likes_number.innerHTML) - 1
+            $post_meta_likes_number.innerHTML = parseInt($post_meta_likes_number.innerHTML) - 1
         }
     })
 }
 
-$post_comment_form_textarea = document.querySelector(".post-comment-form-textarea");
-$post_comment_form_button = document.querySelector(".post-comment-form-button");
-$post_comment_form_textarea.addEventListener("input", () => {
-    if ($post_comment_form_textarea.value != "" && $post_comment_form_button.classList.contains("button-disable")) {
-        $post_comment_form_button.classList.remove("button-disable")
-        $post_comment_form_button.classList.add("button-enable")
-    } else if ($post_comment_form_textarea.value == "" && $post_comment_form_button.classList.contains("button-enable")) {
-        $post_comment_form_button.classList.remove("button-enable")
-        $post_comment_form_button.classList.add("button-disable")
-    }
-})
-
-$post_comment_form = document.querySelector(".post-comment-form");
-
-
-$post_comment_right_update_buttons = document.querySelectorAll(".post-comment-right-update-button")
-
-$post_comment_right_update_buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        $post_comment_form_button.value = "댓글 수정";
-        $post_comment_form_textarea.value = "댓글 내용";
-
+function activateCommentFormButtonWhenExistsInCommentBox() {
+    $post_comment_form_textarea.addEventListener("input", () => {
         if ($post_comment_form_textarea.value != "" && $post_comment_form_button.classList.contains("button-disable")) {
             $post_comment_form_button.classList.remove("button-disable")
             $post_comment_form_button.classList.add("button-enable")
+        } else if ($post_comment_form_textarea.value == "" && $post_comment_form_button.classList.contains("button-enable")) {
+            $post_comment_form_button.classList.remove("button-enable")
+            $post_comment_form_button.classList.add("button-disable")
         }
     })
-})
+}
 
-let post_comment_form = document.querySelector(".post-comment-form");
-preventSubmitIfNotValidate();
 function preventSubmitIfNotValidate() {
     post_comment_form.addEventListener("submit", (event) => {
         if ($post_comment_form_button.classList.contains("button-disable")) {
@@ -85,26 +77,33 @@ function preventSubmitIfNotValidate() {
     })
 }
 
-const modal2 = document.querySelector('.comment-modal');
-const modalOpen2s = document.querySelectorAll('.post-comment-right-delete-button');
-const modalClose2 = document.querySelector('.comment-close_btn');
-const confirm_btn2 = document.querySelector(".comment-confirm_btn");
+function changeCommentButtonToUpdateButton() {
+    $post_comment_right_update_buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            $post_comment_form_button.value = "댓글 수정";
+            $post_comment_form_textarea.value = "댓글 내용";
 
-activateModal2();
+            if ($post_comment_form_textarea.value != "" && $post_comment_form_button.classList.contains("button-disable")) {
+                $post_comment_form_button.classList.remove("button-disable")
+                $post_comment_form_button.classList.add("button-enable")
+            }
+        })
+    })
+}
 
-function activateModal2() {
-    modalOpen2s.forEach( modalopen2 => {
-        modalopen2.addEventListener("click",() => {
-            modal2.style.display = 'block';
+function activateCommentModal() {
+    commentModalOpens.forEach(modalOpen => {
+        modalOpen.addEventListener("click",() => {
+            commentModal.style.display = 'block';
             document.body.style.overflow = 'hidden'
         })
     })
-    modalClose2.addEventListener('click', function () {
-        modal2.style.display = 'none';
+    commentModalClose.addEventListener('click', function () {
+        commentModal.style.display = 'none';
         document.body.style.overflow = ''
     });
 
-    confirm_btn2.addEventListener('click', function () {
+    commentConfirmBtn.addEventListener('click', function () {
         location.href = "./post.html";
         document.body.style.overflow = ''
     })
