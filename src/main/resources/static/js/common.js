7d79bc6a5db020432069ffd9bd7603a1509495e3
jeli01 <jeli01@naver.com>
2025-02-19 02:01:06 +0900

    fix(common.js): 드랍다운메뉴 없는 페이지도 해당 기능 호출하는 문제 해결

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -4,11 +4,13 @@ let $drop_down = document.querySelector(".drop-down");
 activateDropDownMenu();
 
 function activateDropDownMenu() {
-    $header_profile.addEventListener("click", (e) => {
-        if ($drop_down.style.display == "flex") {
-            $drop_down.style.display = "none";
-        } else {
-            $drop_down.style.display = "flex";
-        }
-    })
+    if ($header_profile != null) {
+        $header_profile.addEventListener("click", (e) => {
+            if ($drop_down.style.display == "flex") {
+                $drop_down.style.display = "none";
+            } else {
+                $drop_down.style.display = "flex";
+            }
+        })
+    }
 }
