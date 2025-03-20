3a8ad22b76b7ce7de80a06bbb496a437f6a2f8b7
jeli01 <jeli01@naver.com>
2025-03-20 21:23:52 +0900

    feat: 로그아웃 api 연동 완료

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,7 +1,12 @@
 let $header_profile = document.querySelector(".header-profile");
 let $drop_down = document.querySelector(".drop-down");
+const $logoutButton = document.querySelector(".drop-down-logout");
 
 activateDropDownMenu();
+if ($logoutButton != null) {
+    activateLogoutDropDownMenu();
+}
+
 
 function activateDropDownMenu() {
     if ($header_profile != null) {
@@ -13,4 +18,43 @@ function activateDropDownMenu() {
             }
         })
     }
+}
+
+function activateLogoutDropDownMenu() {
+    $logoutButton.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const token = localStorage.getItem("accessToken");
+        if (!token) {
+            alert("이미 로그아웃 상태입니다.");
+            window.location.href = "./login.html";
+            return;
+        }
+
+        console.log("여기까진 옴1")
+        fetch("http://localhost:8080/logout", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            credentials: "include",
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.isSuccess === "true") {
+                    alert(data.message || "로그아웃 성공!");
+                    localStorage.removeItem("accessToken");
+                    window.location.href = "./login.html";
+                } else {
+                    alert("로그아웃 실패: " + (data.message || ""));
+                }
+            })
+            .catch((err) => {
+                console.error("로그아웃 오류:", err);
+                alert("로그아웃 중 오류가 발생했습니다.");
+            });
+    })
+    console.log("여기까진 옴2")
 }
