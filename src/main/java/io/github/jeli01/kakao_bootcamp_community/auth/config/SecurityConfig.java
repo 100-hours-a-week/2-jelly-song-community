9637518c056885e9a7cee27523a8a153cf98b907
jeli01 <jeli01@naver.com>
2025-03-20 17:24:41 +0900

    feat: 정적 자원에 대해 모든 유저에게 공개

--- src/main/java/io/github/jeli01/kakao_bootcamp_community/auth/config/SecurityConfig.java
+++ src/main/java/io/github/jeli01/kakao_bootcamp_community/auth/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -34,6 +35,11 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/**");
     }
 
     @Bean
