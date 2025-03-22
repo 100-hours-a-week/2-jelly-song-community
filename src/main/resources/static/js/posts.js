9c657c8ae75900aa1e4c755ffc078e055fa3a614
jeli01 <jeli01@naver.com>
2025-03-23 02:15:06 +0900

    feat: 게시물 목록 페이지 api 연동 완료

--- src/main/resources/static/js/posts.js
+++ src/main/resources/static/js/posts.js
@@ -1,7 +1,7 @@
 import {getValidAccessToken, parseJwt} from "./auth.js";
 
 let $headerProfile = document.querySelector(".header-profile");
-
+let $postsContainer = document.querySelector(".layout-main-container");
 
 (async function () {
     try {
@@ -29,6 +29,51 @@ let $headerProfile = document.querySelector(".header-profile");
 
         if (userProfileImageUrl) {
             $headerProfile.style.backgroundImage = `url(${userProfileImageUrl})`;
+        }
+
+        // 게시글 목록 불러오기
+        const boardsRes = await fetch("http://localhost:8080/boards", {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+            },
+        });
+
+        const boardsResult = await boardsRes.json();
+
+        if (boardsResult.isSuccess) {
+            const posts = boardsResult.data;
+            $postsContainer.innerHTML = ""; // 기존 더미 제거
+
+            posts.forEach(post => {
+                const postHTML = `
+                <div class="post-preview" onclick="location.href='./post.html?id=${post.id}'" style="cursor:pointer;">
+                    <div class="post-preview-top">
+                        <div class="post-preview-title-container">
+                            <div class="post-preview-title">${post.title}</div>
+                        </div>
+                        <div class="post-preview-meta-container">
+                            <div class="post-preview-meta">
+                                <div class="post-preview-meta-element">좋아요 ${post.like}</div>
+                                <div class="post-preview-meta-element">댓글 ${post.commentsCounts}</div>
+                                <div class="post-preview-meta-element">조회수 ${post.visitCounts ?? 0}</div>
+                            </div>
+                            <div class="post-preview-time">${post.createDate.replace("T", " ").slice(0, 19)}</div>
+                        </div>
+                    </div>
+                    <div class="post-preview-writer-container">
+                        <div class="preview-profile"></div>
+                        <div class="preview-writer">
+                            <div class="preview-profile-image" style="background-image: url('${post.profileImage}');"></div>
+                            <div class="align-center-container">
+                                ${post.writer}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `;
+                $postsContainer.insertAdjacentHTML("beforeend", postHTML);
+            });
         }
 
     } catch (err) {
