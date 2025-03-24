9a27bc1d5f9b003262d156f17db828d52ff57767
jeli01 <jeli01@naver.com>
2025-03-24 13:36:19 +0900

    refactor: common 패키지 ErrorMessage 제거
    사용되지 않음

--- src/main/java/io/github/jeli01/kakao_bootcamp_community/common/contant/ErrorMessage.java
+++
@@ -1,6 +0,0 @@
-package io.github.jeli01.kakao_bootcamp_community.common.contant;
-
-public class ErrorMessage {
-    public static final String CLIENT_ERROR_MESSAGE = "client error";
-    public static final String SERVER_ERROR_MESSAGE = "internal server error";
-}
