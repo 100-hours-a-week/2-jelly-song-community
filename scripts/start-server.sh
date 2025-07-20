81d69dbebbb4bcab054c2610207e74b94ca99ed5
jeli01 <jeli01@naver.com>
2025-07-20 22:45:59 +0900

    fix(start-server.sh): ecr 레포지토리 주소 올바르게 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,6 @@
 echo "--------------- 서버 배포 시작 -----------------"
 docker stop dev-talk-server || true
 docker rm dev-talk-server || true
-docker pull 145023108970.dkr.ecr.ap-northeast-2.amazonaws.com/dev-talk-server/dev-talk-server:latest
-docker run -d --name dev-talk-server -p 8080:8080 145023108970.dkr.ecr.ap-northeast-2.amazonaws.com/dev-talk-server/dev-talk-server:latest
+docker pull 145023108970.dkr.ecr.ap-northeast-2.amazonaws.com/dev-talk-server:latest
+docker run -d --name dev-talk-server -p 8080:8080 145023108970.dkr.ecr.ap-northeast-2.amazonaws.com/dev-talk-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
